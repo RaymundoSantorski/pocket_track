6589ed7c7d13710f4528b827fdf27ca227eabe15
Raymundo Mateos <raymundo@MacBook-Air-de-Raymundo.local>
2026-05-19 10:34:18 -0600

    add: extra app colors

---
+++ lib/style/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const success = Color(0xFF2ECC71);
+  static const warning = Color(0xFFF5C542);
+  static const cardDark = Color(0xFF1B222C);
+  static const elevatedDark = Color(0xFF242D38);
+  static const subtitleLight = Color(0xFF6B7280);
+  static const subtitleDark = Color(0xFFA1AAB6);
+}
