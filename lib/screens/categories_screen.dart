db7e24390d8a8ef2217845f4f0766ad370d852f3
RaymundoSantorski <rayma9829@gmail.com>
2026-08-18 11:43:46 -0600

    add: navigation to category form

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pocket_track/core/category.dart';
 import 'package:pocket_track/core/category_provider.dart';
+import 'package:pocket_track/screens/category_form.dart';
 import 'package:provider/provider.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -9,7 +10,19 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Category> categories = context.watch<CategoryProvider>().categories;
     return Scaffold(
-      appBar: AppBar(title: Text('Categories')),
+      appBar: AppBar(
+        title: Text('Categories'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (_) => CategoryForm()));
+            },
+          ),
+        ],
+      ),
       body: ListView(
         children: [
           ...categories.map((category) {
