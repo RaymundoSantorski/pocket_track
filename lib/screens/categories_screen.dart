18eae7d9ff7eed70d39ebe6fd6d9efa45cbabad7
RaymundoSantorski <rayma9829@gmail.com>
2026-08-31 12:48:46 -0600

    add: delete category action

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,9 +6,88 @@ import 'package:provider/provider.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  Future<void> confirmDelete(
+    BuildContext context,
+    CategoryProvider db,
+    Category category,
+  ) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Row(
+            children: [
+              Icon(Icons.warning_amber_rounded, color: Colors.redAccent),
+              SizedBox(width: 10),
+              Text('¿Eliminar categoria?'),
+            ],
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('¿Estás seguro de que deseas eliminar esta categoria?'),
+                const SizedBox(height: 10),
+                Text(
+                  'Esta acción no se puede deshacer y perderás los datos relacionados con la categoria',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Theme.of(
+                      context,
+                    ).textTheme.bodyMedium?.color?.withValues(alpha: 0.7),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.grey),
+              ),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+                foregroundColor: Colors.white,
+              ),
+              child: const Text('Eliminar'),
+              onPressed: () async {
+                Navigator.of(context).pop();
+
+                await db.delete(category.id);
+                if (context.mounted) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      persist: false,
+                      duration: Duration(seconds: 3),
+                      content: Text('Transacción eliminada'),
+                      action: SnackBarAction(
+                        label: 'Deshacer',
+                        textColor: Colors.white,
+                        onPressed: () {
+                          db.save(category);
+                        },
+                      ),
+                      backgroundColor: Colors.redAccent,
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     List<Category> categories = context.watch<CategoryProvider>().categories;
+    CategoryProvider db = context.read<CategoryProvider>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Categories'),
@@ -38,15 +117,25 @@ class CategoriesScreen extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [Text(category.name), Text(category.type)],
                       ),
-                      IconButton(
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (_) => CategoryForm(category: category),
-                            ),
-                          );
-                        },
-                        icon: const Icon(Icons.edit),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (_) =>
+                                      CategoryForm(category: category),
+                                ),
+                              );
+                            },
+                            icon: const Icon(Icons.edit),
+                          ),
+                          IconButton(
+                            onPressed: () =>
+                                confirmDelete(context, db, category),
+                            icon: const Icon(Icons.delete),
+                          ),
+                        ],
                       ),
                     ],
                   ),
