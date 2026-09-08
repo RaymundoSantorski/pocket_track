f1bf77da3bb370edb23eb31e03f2f418cee6c310
RaymundoSantorski <rayma9829@gmail.com>
2026-09-07 19:44:10 -0600

    add: edit payment method and delete with confirmation

--- lib/screens/payment_methods_screen.dart
+++ lib/screens/payment_methods_screen.dart
@@ -33,8 +33,88 @@ class _PaymentMethodsScreenState extends State<PaymentMethodsScreen> {
     }
   }
 
+  Future<void> confirmDelete(
+    BuildContext context,
+    PaymentMethodProvider db,
+    PaymentMethod method,
+  ) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Row(
+            children: [
+              Icon(Icons.warning_amber_rounded, color: Colors.redAccent),
+              SizedBox(width: 10),
+              Text('¿Eliminar cliente?'),
+            ],
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(
+                  '¿Estás seguro de que deseas eliminar este metodo de pago?',
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Esta acción no se puede deshacer y perderás los datos relacionados con el metodo de pago',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Theme.of(
+                      context,
+                    ).textTheme.bodyMedium?.color?.withValues(alpha: 0.7),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.grey),
+              ),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+                foregroundColor: Colors.white,
+              ),
+              child: const Text('Eliminar'),
+              onPressed: () async {
+                Navigator.of(context).pop();
+
+                await db.delete(method.id);
+                if (context.mounted) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      persist: false,
+                      duration: Duration(seconds: 3),
+                      content: Text('Transacción eliminada'),
+                      action: SnackBarAction(
+                        label: 'Deshacer',
+                        textColor: Colors.white,
+                        onPressed: () {
+                          db.save(method);
+                        },
+                      ),
+                      backgroundColor: Colors.redAccent,
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    PaymentMethodProvider db = context.read<PaymentMethodProvider>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Payment Methods'),
@@ -80,11 +160,21 @@ class _PaymentMethodsScreenState extends State<PaymentMethodsScreen> {
                     Row(
                       children: [
                         IconButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (_) => AddPaymentMethodScreen(
+                                  paymentMethod: method,
+                                ),
+                              ),
+                            );
+                          },
                           icon: const Icon(Icons.edit),
                         ),
                         IconButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            confirmDelete(context, db, method);
+                          },
                           icon: const Icon(Icons.delete),
                         ),
                       ],
